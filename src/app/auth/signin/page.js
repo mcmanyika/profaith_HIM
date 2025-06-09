384d2e35675d41534bc7e1e1b35a6eae828546a7
mcmanyika <mcmanyika@gmail.com>
2025-06-09 10:34:50 -0500

    update

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -341,13 +341,17 @@ function SignIn() {
               </svg>
               Continue with Google
             </button>
+            
+            <div className="flex items-center justify-center">
+              <span className="text-gray-500">Or</span>
+            </div>
 
             <div className="relative">
               <div className="absolute inset-0 flex items-center">
                 <div className="w-full border-t border-gray-300"></div>
               </div>
               <div className="relative flex justify-center text-sm">
-                <span className="px-2 bg-white text-gray-500">Or continue with email</span>
+                <span className="px-2 bg-white text-gray-500">continue with email</span>
               </div>
             </div>
 
